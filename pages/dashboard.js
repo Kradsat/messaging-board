a654e807b4d3d282a1862fc058db5022f6e6d1de
Kradsat <65266823+Kradsat@users.noreply.github.com>
2022-10-12 19:36:03 +0900

    files

---
+++ pages/dashboard.js
@@ -0,0 +1,82 @@
+import Message from "../components/message";
+import { auth, db } from "../utils/firebase";
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useRouter } from "next/router";
+import { useEffect, useState } from 'react';
+import {
+    collection,
+    query,
+    where,
+    onSnapshot,
+    doc,
+    deleteDoc
+} from "firebase/firestore";
+import { BsTrash2Fill } from "react-icons/bs";
+import { AiFillEdit } from "react-icons/ai";
+import Link from "next/link";
+
+export default function Dashboard() {
+    const route = useRouter();
+    const [user, loading] = useAuthState(auth);
+    const [posts, setPosts] = useState([]);
+
+    //see if user is logged in
+    console.log(user);
+    const getData = async () => {
+        if (loading) return;
+        if (!user) return route.push('/auth/login');
+        const collectionRef = collection(db, "posts");
+        const q = query(collectionRef, where("user", "==", user.uid));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        });
+        return unsubscribe;
+    };
+
+    // delete post
+    const deletePost = async (id) => {
+        const docRef = doc(db, "posts", id);
+        await deleteDoc(docRef);
+    }
+
+    //get user data
+    useEffect(() => {
+        getData();
+    }, [user, loading]);
+
+    return (
+        <div>
+            <h1>Your Posts</h1>
+            <div>
+                {posts.map(post => {
+                    return (
+                        <Message key={post.id} {...post}>
+                            <div className="flex gap-4">
+                                <button
+                                    onClick={() => deletePost(post.id)}
+                                    className=" text-pink-600 flex item justify-center gap-2 py-2"
+                                >
+                                    <BsTrash2Fill className=" text-2xl" />
+                                    Delete
+                                </button>
+                                <Link href={{ pathname: "/post", query: post }}>
+                                    <button
+                                        className=" text-teal-600 flex item justify-center gap-2 py-2"
+                                    >
+                                        <AiFillEdit className=" text-2xl" />
+                                        Edit
+                                    </button>
+                                </Link>
+
+                            </div>
+                        </Message>
+                    );
+                })}
+            </div>
+            <button className=" font-medium text-white bg-gray-800 py-2 px-4 my-6 rounded-lg"
+                onClick={() => auth.signOut()}
+            >Sign Out
+            </button>
+        </div>
+    );
+}
